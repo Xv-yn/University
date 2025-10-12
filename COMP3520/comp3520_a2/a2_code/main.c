ae68a70194817155d226aca033e5d98b9226eecc
Xv-yn <elwoodfong@gmail.com>
2025-10-12 22:23:24 +1100

    updated assignment to meet spec req

--- COMP3520/comp3520_a2/a2_code/main.c
+++ COMP3520/comp3520_a2/a2_code/main.c
@@ -128,9 +128,9 @@ static void ensure_started(Job *j){
     if(pid == 0){
         char idbuf[32];
         snprintf(idbuf,sizeof(idbuf),"%d", j->id);
-        char *argv[] = { "./worker", idbuf, NULL };
-        execv("./worker", argv);
-        perror("execv ./worker");
+        char *argv[] = { "./sigtrap", idbuf, NULL };
+        execv("./sigtrap", argv);
+        perror("execv ./sigtrap");
         _exit(127);
     }
     j->pid = pid;
